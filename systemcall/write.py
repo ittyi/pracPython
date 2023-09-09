12e0e86d223775760d3af3252f96b69b6f251791
ittyi <vioiv.k4@gmail.com>
2023-09-10 08:31:10 +0900

    ⚗️ writeを色々弄ってみた

--- systemcall/write.py
+++ systemcall/write.py
@@ -1,3 +1,13 @@
 import os
 
 os.write(1, b'h')
+tmp = "Hello, World!"
+print(tmp)
+print(len(tmp))
+
+for i in range(len(tmp)):
+    # print(i)
+    # print(type(i))
+    # print("i.encode('UTF-8')", tmp[i].encode('UTF-8'))
+    os.write(1, tmp[i].encode('UTF-8'))
+
