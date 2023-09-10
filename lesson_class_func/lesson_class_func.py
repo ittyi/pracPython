64d78fb6f0a845d279058e125f5907da822474eb
ittyi <vioiv.k4@gmail.com>
2023-09-10 09:48:58 +0900

    ✨ 名簿出力機能の実装完成

--- lesson_class_func/lesson_class_func.py
+++ lesson_class_func/lesson_class_func.py
@@ -13,10 +13,10 @@ def main():
     meibo()
 
 class Person:
-    def __init__(self, record):                  # コンストラクタ
+    def __init__(self, record): # コンストラクタ
         self.name = record[0]
-        self.height = record[1]
-        self.weight = record[2]
+        self.height = int(record[1])
+        self.weight = int(record[2])
         self.footsize = record[3]
         self.profession = record[4]
         self.working_history = record[5]
@@ -30,6 +30,22 @@ class Person:
         print("profession:", self.profession)
         print("working_history:", self.working_history)
         print("publishing_settings:", self.publishing_settings)
+    
+    def print_height(self):
+        print(self.name+"さん"+"の"+"身長"+"は"+str(self.height)+"(cm)"+"です。")
+    
+    def print_weight(self):
+        print(self.name+"さん"+"の"+"体重"+"は"+str(self.weight)+"(kg)"+"です。")
+    
+    def print_footsize(self):
+        print(self.name+"さん"+"の"+"足のサイズ"+"は"+str(self.footsize)+"です。")
+    
+    def print_profession(self):
+        print(self.name+"さん"+"の"+"職業"+"は"+str(self.profession)+"です。")
+
+    def print_working_history(self):
+        print(self.name+"さん"+"の"+"職歴"+"は"+str(self.working_history)+"です。")
+    
 
 
 def meibo():
@@ -45,14 +61,23 @@ def meibo():
     # print(s)
     meibo_list = s.split('\n')
     # print(meibo_list)
+    meibo = []
     for record in meibo_list:
         if record == '':
             continue
         print(record)
         tmp = record.split()
         print(tmp)
-        Person(tmp).properties()
+        meibo.append(Person(tmp))
 
+    print(meibo)
+    for person in meibo:
+        person.properties()
+        person.print_height()
+        person.print_weight()
+        person.print_footsize()
+        person.print_profession()
+        person.print_working_history()
 
     f.close()
 
