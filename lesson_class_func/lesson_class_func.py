22e8bbe45a2f1f336e6a93d5c9fb0d9db99bc246
ittyi <vioiv.k4@gmail.com>
2023-09-10 10:06:57 +0900

    ✨ 公開非公開による全出力の切り替えに対応

--- lesson_class_func/lesson_class_func.py
+++ lesson_class_func/lesson_class_func.py
@@ -4,13 +4,12 @@ def main():
     print("Hello World!")
     args = sys.argv
 
-    print(args[0])
     print(args[1])
-    print(args[2])
+    # print(args[2])
     if args[1] == '-h':
         print("-h in Hello World!")
-    
-    meibo()
+    elif args[1] == '-m':
+        meibo()
 
 class Person:
     def __init__(self, record): # コンストラクタ
@@ -20,7 +19,7 @@ class Person:
         self.footsize = record[3]
         self.profession = record[4]
         self.working_history = record[5]
-        self.publishing_settings = record[6]
+        self.publishing_settings = str(record[6])
     
     def properties(self):
         print("name:", self.name)
@@ -46,41 +45,48 @@ class Person:
     def print_working_history(self):
         print(self.name+"さん"+"の"+"職歴"+"は"+str(self.working_history)+"です。")
     
-
+    def is_public(self):
+        if self.publishing_settings == "公開":
+            return True
+        else:
+            return False
 
 def meibo():
     print("in meibo()")
+
+    # file open&input part
     path = './meibo.txt'
-
     f = open(path)
-
-    print(type(f))
-    # <class '_io.TextIOWrapper'>
     s = f.read()
-    # print(type(s))
-    # print(s)
+
+    # Input processing part
     meibo_list = s.split('\n')
-    # print(meibo_list)
     meibo = []
     for record in meibo_list:
         if record == '':
             continue
-        print(record)
         tmp = record.split()
-        print(tmp)
         meibo.append(Person(tmp))
 
-    print(meibo)
-    for person in meibo:
-        person.properties()
+    # output part
+    print(str(len(meibo))+"人の名簿を受け取りました。")
+    all_print(meibo)
+
+    # file close
+    f.close()
+
+def all_print(persons: Person):
+    for person in persons:
+        # print(person)
+        if person.is_public() == False:
+            continue
+        # person.properties()
         person.print_height()
         person.print_weight()
         person.print_footsize()
         person.print_profession()
         person.print_working_history()
 
-    f.close()
 
-        
 if __name__ == "__main__":
     main()
